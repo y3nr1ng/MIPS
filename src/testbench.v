36d8568a4ce1a6088b3a9bea0c86b340b70561f1
Liu Yen-Ting <windows.linux.mac@gmail.com>
2015-11-26 21:36:40 +0800

    Modify the file paths in the testbench

--- src/testbench.v
+++ src/testbench.v
@@ -1,4 +1,4 @@
-`define CYCLE_TIME 50            
+`define CYCLE_TIME 50
 
 module TestBench;
 
@@ -7,63 +7,63 @@ reg                Start;
 integer            i, outfile, counter;
 integer            stall, flush;
 
-always #(`CYCLE_TIME/2) Clk = ~Clk;    
+always #(`CYCLE_TIME/2) Clk = ~Clk;
 
 CPU CPU(
     .clk_i  (Clk),
     .start_i(Start)
 );
-  
+
 initial begin
     counter = 0;
     stall = 0;
     flush = 0;
-    
+
     // initialize instruction memory
     for(i=0; i<256; i=i+1) begin
         CPU.Instruction_Memory.memory[i] = 32'b0;
     end
-    
+
     // initialize data memory
     for(i=0; i<32; i=i+1) begin
         CPU.Data_Memory.memory[i] = 8'b0;
-    end    
-        
+    end
+
     // initialize Register File
     for(i=0; i<32; i=i+1) begin
         CPU.Registers.register[i] = 32'b0;
     end
-    
+
     // Load instructions into instruction memory
-    $readmemb("instruction.txt", CPU.Instruction_Memory.memory);
-    
+    $readmemb("..\dat\instruction.txt", CPU.Instruction_Memory.memory);
+
     // Open output file
-    outfile = $fopen("output.txt") | 1;
-    
+    outfile = $fopen("..\dat\output.txt") | 1;
+
     // Set Input n into data memory at 0x00
     CPU.Data_Memory.memory[0] = 8'h5;       // n = 5 for example
-    
+
     Clk = 1;
     //Reset = 0;
     Start = 0;
-    
-    #(`CYCLE_TIME/4) 
+
+    #(`CYCLE_TIME/4)
     //Reset = 1;
     Start = 1;
-        
-    
+
+
 end
-  
+
 always@(posedge Clk) begin
     if(counter == 30)    // stop after 30 cycles
         $finish;
 
     // put in your own signal to count stall and flush
     // if(CPU.HazzardDetection.mux8_o == 1 && CPU.Control.Jump_o == 0 && CPU.Control.Branch_o == 0)stall = stall + 1;
-    // if(CPU.HazzardDetection.Flush_o == 1)flush = flush + 1;  
+    // if(CPU.HazzardDetection.Flush_o == 1)flush = flush + 1;
     // print PC
     $fdisplay(outfile, "cycle = %d, Start = %d, Stall = %d, Flush = %d\nPC = %d", counter, Start, stall, flush, CPU.PC.pc_o);
-    
+
     // print Registers
     $fdisplay(outfile, "Registers");
     $fdisplay(outfile, "R0(r0) = %d, R8 (t0) = %d, R16(s0) = %d, R24(t8) = %d", CPU.Registers.register[0], CPU.Registers.register[8] , CPU.Registers.register[16], CPU.Registers.register[24]);
@@ -84,13 +84,13 @@ always@(posedge Clk) begin
     $fdisplay(outfile, "Data Memory: 0x14 = %d", {CPU.Data_Memory.memory[23], CPU.Data_Memory.memory[22], CPU.Data_Memory.memory[21], CPU.Data_Memory.memory[20]});
     $fdisplay(outfile, "Data Memory: 0x18 = %d", {CPU.Data_Memory.memory[27], CPU.Data_Memory.memory[26], CPU.Data_Memory.memory[25], CPU.Data_Memory.memory[24]});
     $fdisplay(outfile, "Data Memory: 0x1c = %d", {CPU.Data_Memory.memory[31], CPU.Data_Memory.memory[30], CPU.Data_Memory.memory[29], CPU.Data_Memory.memory[28]});
-	
+
     $fdisplay(outfile, "\n");
-    
+
     counter = counter + 1;
-    
-      
+
+
 end
 
-  
+
 endmodule
