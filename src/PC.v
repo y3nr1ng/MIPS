a45ac497d4e31417edecc37e022abb33f6372432
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-02 16:08:00 +0800

    Change the way pins are defined

--- src/PC.v
+++ src/PC.v
@@ -1,22 +1,11 @@
 module PC
 (
-    clk_i,
-    rst_i,
-    start_i,
-    pc_i,
-    pc_o
+    input		clk_i,
+    input		rst_i,
+    input		start_i,
+    input	[31:0]	pc_i,
+    output reg	[31:0]	pc_o
 );
-
-// Ports
-input               clk_i;
-input               rst_i;
-input               start_i;
-input   [31:0]      pc_i;
-output  [31:0]      pc_o;
-
-// Wires & Registers
-reg     [31:0]      pc_o;
-
 
 always@(posedge clk_i or negedge rst_i) begin
     if(~rst_i) begin
