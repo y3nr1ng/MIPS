f348f168eaea34c3ba0d0f93d30aba5e7ccaf7df
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-02 22:32:30 +0800

    Change from ROM design to simple lookup table

---
+++ src/LookupTable.v
@@ -0,0 +1,30 @@
+`ifndef LUT
+`define LUT
+
+parameter ADD.op	= 000000;
+parameter ADD.func	= 100000;
+
+parameter SUB.op	= 000000;
+parameter SUB.func	= 100010;
+
+parameter MUL.op	= 000000;
+parameter MUL.func	= 011000;
+
+parameter ADDI.op	= 001000;
+ 
+parameter SW.op		= 101011;
+
+parameter LW.op		= 100011;
+
+parameter J.op		= 000010;
+
+parameter BEQ.op
+
+parameter AND.op	= 000000;
+parameter AND.func	= 100100;
+
+parameter OR.op		= 000000;
+parameter OR.func	= 100101;
+
+
+`endif
