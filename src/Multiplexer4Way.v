c09fd0132b0563e8cae35ea8189e29f538f4cadc
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-03 10:37:54 +0800

    Add 4-way mux for ALU inputs

---
+++ src/Multiplexer4Way.v
@@ -0,0 +1,17 @@
+module Multiplexer4Way #(parameter width=32) (
+	input		[width-1:0]	data_1, data_2, data_3, data_4,
+	input 		[1:0]		sel_i,
+	output reg 	[width-1:0]	data_o
+);
+	
+	always @ (*)
+	begin
+		case (sel_i)
+			2'b00	:	data_o = data_1;
+			2'b01	:	data_o = data_2;
+			2'b10	:	data_o = data_3;
+			2'b11	:	data_o = data_4;
+		endcase
+	end
+
+endmodule
