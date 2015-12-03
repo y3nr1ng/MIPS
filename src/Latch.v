210a5c143a7784dc796f25172ac37ad6170bdba6
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-03 11:30:05 +0800

    Implement latch for pipeline registers

---
+++ src/Latch.v
@@ -0,0 +1,15 @@
+module latch #(parameter width=32) (
+	input					clk,
+	input      				rst,
+	input      				en,
+	input      	[width-1:0]	data_i,
+	output reg 	[width-1:0]	data_o
+);
+
+	always@(posedge clk or negedge rst)
+  		if (!rst)
+   			data_o <= { width{1'b0} };
+  		else if (en)
+    		data_o <= data_i;
+
+endmodule
