b8b61d57b459142ec4106108fb914d390ef65b25
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-03 10:44:02 +0800

    Rename the multiplexer to identify between 4/2 ways

---
+++ src/Multiplexer2Way.v
@@ -0,0 +1,15 @@
+module Multiplexer2Way #(parameter width=32) (
+	input		[width-1:0]	data_1, data_2,
+	input 					sel_i,
+	output reg	[width-1:0]	data_o
+);
+	
+	always @ (*)
+	begin
+		case (sel_i)
+			1'b0	:	data_o = data_1;
+			1'b1	:	data_o = data_2;
+		endcase
+	end
+
+endmodule
