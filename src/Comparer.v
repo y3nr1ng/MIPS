c18f8caa24ee0bce22971ce6c72b7765a67512a6
Chia-sheng Chen <qitar888@gmail.com>
2015-12-04 13:47:18 +0800

    Fix typo mistake on comment

--- src/Comparer.v
+++ src/Comparer.v
@@ -4,7 +4,7 @@ module Comparer #(parameter width=32) (
 );
 	
 	// == should be used here instead of ===,
-	// since Hi-Z and float should yield ambiguous reuslt.
+	// since Hi-Z and float should yield ambiguous result.
 	assign is_equal = (data_1 == data_2);
 	assign is_greater = (data_1 > data_2);
 	assign is_less = (data_1 < data_2);
