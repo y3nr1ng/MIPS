ad7095bb8f71a8b198c20864a62000711d75f6f4
Even <even841130@gmail.com>
2015-12-04 09:51:03 +0800

    Implement forwarding unit

--- src/ForwardingUnit.v
+++ src/ForwardingUnit.v
@@ -1,11 +1,20 @@
 module ForwardingUnit(
-	input	[]	IDEX_Rs,
-	input	[]	IDEX_Rt,
-	input	[]	EXMEM_Rd,
-	input	[]	MEMWB_Rd,
+	input	EXMEM_rw,
+	input	MEMWB_rw,
+	input	[0:5]	IDEX_Rs,
+	input	[0:5]	IDEX_Rt,
+	input	[0:5]	EXMEM_Rd,
+	input	[0:5]	MEMWB_Rd,
 	output	[0:1]	ALUdata1_sel,
 	output	[0:1]	ALUdata2_sel
 );
 
+assign ALUdata1_sel = 	(EXMEM_rw == 1 && EXMEM_Rd != 0 && EXMEM_Rd == IDEX_Rs)? 10:
+						(MEMWB_rw == 1 && MEMWB_Rd != 0 && MEMWB_Rd == IDEX_Rs)? 01:
+						00;
+
+assign ALUdata2_sel =	(EXMEM_rw == 1 && EXMEM_Rd != 0 && EXMEM_Rd == IDEX_Rt)? 10:
+						(MEMWB_rw == 1 && MEMWB_Rd != 0 && MEMWB_Rd == IDEX_Rt)? 01:
+						00;
 
 endmodule
