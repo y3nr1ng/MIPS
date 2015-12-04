deac42e13401c13280f56864a873484c41736347
Even <even841130@gmail.com>
2015-12-04 10:09:26 +0800

    Forwarding unit modifies wrong in wire number

--- src/ForwardingUnit.v
+++ src/ForwardingUnit.v
@@ -1,10 +1,10 @@
 module ForwardingUnit(
 	input	EXMEM_rw_i,
 	input	MEMWB_rw_i,
-	input	[5:0]	IDEX_Rs_i,
-	input	[5:0]	IDEX_Rt_i,
-	input	[5:0]	EXMEM_Rd_i,
-	input	[5:0]	MEMWB_Rd_i,
+	input	[4:0]	IDEX_Rs_i,
+	input	[4:0]	IDEX_Rt_i,
+	input	[4:0]	EXMEM_Rd_i,
+	input	[4:0]	MEMWB_Rd_i,
 	output	[1:0]	ALUdata1_sel_o,
 	output	[1:0]	ALUdata2_sel_o
 );
