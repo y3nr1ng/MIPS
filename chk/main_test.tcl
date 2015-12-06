9bf0df227509036a3711a09ffc81f8ee21499a6b
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-07 02:08:43 +0800

    Rearrange the signals

--- chk/main_test.tcl
+++ chk/main_test.tcl
@@ -4,19 +4,15 @@ add wave -noupdate -divider Timing
 add wave -noupdate -label Start /TestBench/CPU/PC/start
 add wave -noupdate -label Reset /TestBench/CPU/PC/rst
 add wave -noupdate -label Clock /TestBench/CPU/PC/clk
-add wave -noupdate -divider {Program Counter}
-add wave -noupdate -label {Current Address} -radix unsigned -radixshowbase 0 /TestBench/CPU/PC/addr_o
-add wave -noupdate -label {Write Enable} /TestBench/CPU/PC/we
-add wave -noupdate -divider Instruction
-add wave -noupdate -label Instruction /TestBench/CPU/IFID_PC_Inc/data_o
-add wave -noupdate /TestBench/CPU/instr
-add wave -noupdate -expand -group {Parsed Instruction} -label {Rs Address} -radix unsigned -radixshowbase 0 /TestBench/CPU/RegFiles/Rs_addr
-add wave -noupdate -expand -group {Parsed Instruction} -label {Rt Address} -radix unsigned -radixshowbase 0 /TestBench/CPU/RegFiles/Rt_addr
-add wave -noupdate -expand -group {Parsed Instruction} -label {I-type Immediate} -radix decimal /TestBench/CPU/SignExt/data_i
-add wave -noupdate -divider {Branch Control}
-add wave -noupdate -label {R[Rs] == R[Rt]?} /TestBench/CPU/Rs_eq_Rt/is_equal
-add wave -noupdate -label {Branch Target Address} -radix decimal /TestBench/CPU/PC_Mux/data_4
-add wave -noupdate -label {Jump Target Address} -radix decimal -radixshowbase 0 /TestBench/CPU/PC_Mux/data_3
+add wave -noupdate -expand -group IF -label {Current Address} -radix unsigned -radixshowbase 0 /TestBench/CPU/PC/addr_o
+add wave -noupdate -expand -group IF -label {Write Enable} /TestBench/CPU/PC/we
+add wave -noupdate -expand -group ID -label Instruction /TestBench/CPU/instr
+add wave -noupdate -expand -group ID -expand -group {Parsed Instruction} -label {Rs Address} -radix unsigned -radixshowbase 0 /TestBench/CPU/RegFiles/Rs_addr
+add wave -noupdate -expand -group ID -expand -group {Parsed Instruction} -label {Rt Address} -radix unsigned -radixshowbase 0 /TestBench/CPU/RegFiles/Rt_addr
+add wave -noupdate -expand -group ID -expand -group {Parsed Instruction} -label {I-type Immediate} -radix decimal /TestBench/CPU/SignExt/data_i
+add wave -noupdate -expand -group ID -label {R[Rs] == R[Rt]?} /TestBench/CPU/Rs_eq_Rt/is_equal
+add wave -noupdate -expand -group ID -label {Branch Target Address} -radix decimal /TestBench/CPU/PC_Mux/data_4
+add wave -noupdate -expand -group ID -label {Jump Target Address} -radix decimal -radixshowbase 0 /TestBench/CPU/PC_Mux/data_3
 TreeUpdate [SetDefaultTree]
 WaveRestoreCursors {{Cursor 1} {720 ps} 0}
 quietly wave cursor active 1
@@ -34,4 +30,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {0 ps} {782 ps}
+WaveRestoreZoom {257 ps} {1039 ps}
