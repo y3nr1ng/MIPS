f09781468a1218113244444958529a4ab3c655fb
Yen-Ting Liu <windows.linux.mac@gmail.com>
2015-12-07 01:04:23 +0800

    Register now functioning

--- src/SingleCycleCPU.v
+++ src/SingleCycleCPU.v
@@ -7,6 +7,7 @@ module SingleCycleCPU
 	input		start
 );
 
+
 /**
  * IF
  */
@@ -25,6 +26,8 @@ Adder PC_Inc (
 	.data_o			()
 );
 
+wire		[31:0]	instr;
+
 // Instruction memory acts as a ROM.
 Memory #(.size(1024)) InstrMem (
 	.clk			(clk),
@@ -32,7 +35,32 @@ Memory #(.size(1024)) InstrMem (
 	.cs				(1'b1),
 	.we				(1'b0),
 	.data_i			(),
-	.data_o			()
+	.data_o			(instr)
+);
+
+
+/**
+ * ID
+ */
+wire		[5:0]	instr_op	= instr[31:26];
+wire		[5:0]	instr_func	= instr[5:0];
+
+wire		[4:0]	instr_rs	= instr[25:21];
+wire		[4:0]	instr_rt	= instr[20:16];
+wire		[4:0]	instr_rd	= instr[15:11];
+
+wire		[15:0]	instr_imm	= instr[15:0];
+wire		[25:0]	addr_imm 	= instr[25:0];
+
+Registers RegFiles (
+	.clk			(clk),
+	.Rs_addr		(instr_rs),
+	.Rt_addr		(instr_rt),
+	.Rs_data		(),
+	.Rt_data		(),
+	.we				(1'b0),	// TODO
+	.Rd_addr		(),
+	.Rd_data		()
 );
 
 endmodule
