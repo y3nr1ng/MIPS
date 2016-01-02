ed8c68880f58ccc3e3c69b57d037ef45b797ef7f
Yen-Ting Liu <windows.linux.mac@gmail.com>
2016-01-02 10:40:00 +0800

    Init the cache controller to zero

--- L1_Cache.v
+++ L1_Cache.v
@@ -1,12 +1,19 @@
 module L1_Cache
+#(
+	parameter width 	= 32
+)
 (
 	input					clk,
-	input		[32-1:0]	addr_i,
+	input		[width-1:0]	addr_i,
 	input					cs,
 	input					we,
-	input		[32-1:0]	data_i,
-	output	reg	[32-1:0]	data_o,
+	input		[width-1:0]	data_i,
+	output	reg	[width-1:0]	data_o,
 	output	reg				stall
 );
+
+	initial begin
+		data_o = { width{1'bz} };
+	end
 
 endmodule
