a84e356a04bcde44a566bbe4aef67b4cd653744b
Even <even841130@gmail.com>
2016-01-03 04:12:26 +0800

    Modified L1 Cache controller interface

--- src/L1_Cache.v
+++ src/L1_Cache.v
@@ -66,15 +66,23 @@ module L1_Cache
 	end
 
 	// module instantiations.
+	reg		p1require;
+	assign p1require = cs | we ;
+
 	L1_Cache_Controller controller
 	(
 		.clk			(clk),
 		.rst			(rst),
+		.require		(p1require),
+		.p_mem_we		(we),
 		.cache_hit		(cache_hit),
 		.cache_dirty	(cache_dirty),
 		.sram_cs		(),
 		.sram_we		(),
-		.stall			()
+		.stall			(stall),
+		.mem_cs			(CPU.ext_mem_cs),
+		.mem_we 		(CPU.ext_mem_we),
+		.mem_wb 		()
 	);
 
 	SRAM #(.addr_width(5), .data_width(24), .mem_size(32)) tag_mem
