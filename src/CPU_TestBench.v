7dd71f2df559044f0019fb2e7f6288dd0f2e1564
Yen-Ting Liu <windows.linux.mac@gmail.com>
2016-01-02 15:06:40 +0800

    Add main memory module

--- src/CPU_TestBench.v
+++ src/CPU_TestBench.v
@@ -10,10 +10,25 @@ integer     stall, flush;
 
 always #(`CYCLE_TIME/2) clk = ~clk;
 
-CPU CPU(
-    .clk	(clk),
-	.rst	(reset),
-    .start	(start)
+CPU CPU (
+    .clk			(clk),
+	.rst			(reset),
+    .start			(start),
+	.ext_mem_data_i	(),
+	.ext_mem_ack	(),
+	.ext_mem_data_o	(),
+	.ext_mem_addr	(),
+	.ext_mem_cs		(),
+	.ext_mem_we		()
+);
+
+DRAM memory (
+	.clk			(clk),
+	.addr_i			(),
+	.cs				(),
+	.we				(),
+	.data_i			(),
+	.data_o			()
 );
 
 initial begin
@@ -75,7 +90,7 @@ always@(posedge clk) begin
     // print PC
     $fdisplay(outfile, "cycle = %d, start = %d, Stall = %d, Flush = %d\nPC = %d", counter, start, stall, flush, CPU.PC.addr_o);
 
-    // print RegFiles
+    // print registers 
     $fdisplay(outfile, "RegFiles");
     $fdisplay(outfile, "R0(r0) = %d, R8 (t0) = %d, R16(s0) = %d, R24(t8) = %d", CPU.RegFiles.register[0], CPU.RegFiles.register[8] , CPU.RegFiles.register[16], CPU.RegFiles.register[24]);
     $fdisplay(outfile, "R1(at) = %d, R9 (t1) = %d, R17(s1) = %d, R25(t9) = %d", CPU.RegFiles.register[1], CPU.RegFiles.register[9] , CPU.RegFiles.register[17], CPU.RegFiles.register[25]);
@@ -86,7 +101,7 @@ always@(posedge clk) begin
     $fdisplay(outfile, "R6(a2) = %d, R14(t6) = %d, R22(s6) = %d, R30(s8) = %d", CPU.RegFiles.register[6], CPU.RegFiles.register[14], CPU.RegFiles.register[22], CPU.RegFiles.register[30]);
     $fdisplay(outfile, "R7(a3) = %d, R15(t7) = %d, R23(s7) = %d, R31(ra) = %d", CPU.RegFiles.register[7], CPU.RegFiles.register[15], CPU.RegFiles.register[23], CPU.RegFiles.register[31]);
 
-    // print Data Memory
+    // print data memory
     $fdisplay(outfile, "Data Memory: 0x00 = %d", {CPU.DataMem.memory[3] , CPU.DataMem.memory[2] , CPU.DataMem.memory[1] , CPU.DataMem.memory[0] });
     $fdisplay(outfile, "Data Memory: 0x04 = %d", {CPU.DataMem.memory[7] , CPU.DataMem.memory[6] , CPU.DataMem.memory[5] , CPU.DataMem.memory[4] });
     $fdisplay(outfile, "Data Memory: 0x08 = %d", {CPU.DataMem.memory[11], CPU.DataMem.memory[10], CPU.DataMem.memory[9] , CPU.DataMem.memory[8] });
