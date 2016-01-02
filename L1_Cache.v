5a2765032288074aacfc7ad165462542192fe775
Yen-Ting Liu <windows.linux.mac@gmail.com>
2016-01-02 10:38:48 +0800

    Adapt L1 cache module to new interface

--- L1_Cache.v
+++ L1_Cache.v
@@ -1,5 +1,12 @@
 module L1_Cache
 (
+	input					clk,
+	input		[32-1:0]	addr_i,
+	input					cs,
+	input					we,
+	input		[32-1:0]	data_i,
+	output	reg	[32-1:0]	data_o,
+	output	reg				stall
 );
 
 endmodule
