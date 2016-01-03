d5cf459035109da73ce11250dc7fbdf72db1a662
Yen-Ting Liu <windows.linux.mac@gmail.com>
2016-01-03 16:10:12 +0800

    Fix typo in L1 cache controller rework

---
+++ src/L1_Cache_Controller_rework.v
@@ -0,0 +1,27 @@
+`include "StateTable.v"
+
+module L1_Cache_Controller (
+	// interface to CPU
+	input			cache_cs,
+	input			cache_we,
+	output reg		cache_ack,
+
+	// interface to internal components
+	input			cache_hit,
+	output reg		sram_we,
+	input			cache_valid,
+	input			cache_dirty_i,
+	output reg		cache_dirty_o,
+	output reg		dram_data_sel,
+	output reg		cpu_data_sel,
+	
+	// interface to DRAM
+	output reg		dram_addr_sel,
+	output reg		dram_cs,
+	output reg		dram_we,
+	output reg		dram_ack
+);
+
+
+
+endmodule
